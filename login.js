e7eab64c265983a6ae57751772de5e167cce74ab
Fernando Maldonado <fermaldonadosolis@gmail.com>
2022-09-20 16:40:12 -0500

    working on create user

--- login.js
+++ login.js
@@ -6,9 +6,19 @@ const userPass = "mamarre";
 const loginForm = document.getElementById("loginForm");
 const inputEmail = document.getElementById("email");
 const inputPass = document.getElementById("pass");
+const createNewUser = document.getElementById("createNewUser");
+const inputNewPass = document.getElementById("passCreate");
+const inputNewEmail = document.getElementById("userCreate");
+const roleRadio = document.querySelector('input[name="flexRadioDefault"]:checked').value;
 /*-----------------------------------------*/          
 
+
+
+
+
 let users = {};
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
                                                     if (localStorage.getItem("books"))	{// if there's an object on localStorage we retrieve it from there
@@ -17,12 +27,19 @@ document.addEventListener("DOMContentLoaded", () => {
                                                                                         }
                                                     });
 
-
+createNewUser.addEventListener("submit", e => { // listens Add button
+                                        e.preventDefault(); //prevents default behavior
+                                        addUser(e); // add the book
+                                        });
 
 loginForm.addEventListener("submit", e =>   { // listens the submit button of the page
                                             e.preventDefault();
                                             startSession(e);
                                             });
+
+loginForm.addEventListener("click", e =>{
+                                        createUser(e);
+                                        })                                            
 
 const startSession = e =>   {
                             if (inputEmail.value == users[1].email && inputPass.value == users[1].password) { // if the mail and password matches
@@ -44,3 +61,26 @@ const startSession = e =>   {
                             loginForm.reset();
                             }
                                               
+
+const createUser = e => {
+                        if (e.target.classList.contains("btn-warning")) { 
+                                                                        (function(){
+                                                                            $(function(){
+                                                                                $("#modalUser").modal()
+                                                                            });
+                                                                        }());
+                                                                        return;
+                                                                        }
+                        }                            
+
+const addUser = e =>{
+                    const user ={
+                                userEmail: inputEmail.value,
+                                userPass: inputPass.value,
+                                }
+                    console.log(user);
+                    console.log(roleRadio);
+
+/* books[book.id] = book; // we push the book into our bookshelf */
+addForm.reset(); // we reset our form
+                    }                        
